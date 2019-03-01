fad4e4a6af535cecece1e090db773614ca72317d
Alexandre Kouekam <kouek.alexandre@gmail.com>
2019-03-01 21:57:48 +0100

    Add tab to app and WIP adding data to list fav

---
+++ Components/Favorite.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Favorite extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>Mon Onglet Favorie</Text>
+            </View>
+        )
+    }
+}
+
+export default Favorite
